45cb81925728ec9f6ed69a07c20a3c1066c11a46
KateLe239 <79245648+KateLe239@users.noreply.github.com>
2021-06-23 13:29:49 +0300

    Update RandomClient.kt

--- 02-sums-random-client/src/main/kotlin/RandomClient.kt
+++ 02-sums-random-client/src/main/kotlin/RandomClient.kt
@@ -1,3 +1,9 @@
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import java.net.InetSocketAddress
 import kotlin.random.Random
 
 /*
@@ -5,7 +11,23 @@ import kotlin.random.Random
 формирует запрос с числами на сервер, отправляет его, получает от сервера и печатает результат.
 Всего клиент должен посылать 100 запросов.
  */
+fun main() = runBlocking {
+    val hostname = "127.0.0.1"
+    val port = 2323
+    val socket = aSocket(ActorSelectorManager(Dispatchers.IO)).tcp().connect(InetSocketAddress(hostname, port))
+    val input = socket.openReadChannel()
+    val output = socket.openWriteChannel(autoFlush = true)
 
-fun main(args: Array<String>) {
-    TODO()
+    for(i in 1..100) {
+        val k: Int = Random.nextInt(10)
+        var out: String = Random.nextInt(10000).toString()
+        for(i in 2..k){
+            out += ' '
+            out += Random.nextInt(10000).toString()
+        }
+        println(out)
+        output.writeStringUtf8("$out\r\n")
+        val response = input.readUTF8Line()
+        println("Server said: '$response'")
+    }
 }
